80dea3a2fba5c9d96b38b3aec5cc613ee158baa9
xsip <xsip@pm.me>
2025-02-28 00:55:29 +0100

    cleaning

--- CS2-SchemaSystem-Dumper/main.cpp
+++ CS2-SchemaSystem-Dumper/main.cpp
@@ -10,9 +10,6 @@
 #include "memory.h"
 #include "CSchemaSystem.h"
 #include  "SchemaLoader.hpp"
-#include "ClientSchemaLoader.hpp"
-#include "Engine2SchemaLoader.hpp"
-#include "ServerSchemaLoader.hpp"
 #include "AllLoader.hpp"
 class ModuleData{
 public:
@@ -45,14 +42,22 @@ int main(int argc, char* argv[]) {
 
 	for (int i = 0; i < pSchemaSystem->m_nScopeSize; i++) {
 		auto currentScope = pSchemaSystem->m_pScopeArray[i];
-		printf("Current Scope: %s\n", currentScope->m_szName);
+		
 
 		if (currentScope->m_nNumDeclaredClasses == 65535)
 			continue;
 
 		for (int classDef = 0; classDef < currentScope->m_nNumDeclaredClasses; classDef++) {
 			auto currentClassDef = currentScope->m_pDeclaredClasses[classDef];
-			printf("\tClassDefinition: %s\n", currentClassDef.m_pDeclaredClass->m_szName);
+			if (strstr(currentClassDef.m_pDeclaredClass->m_szName, "C_BaseEntity")) {
+				auto classData = currentClassDef.m_pDeclaredClass->m_Class;
+				auto numFields = classData->m_nNumFields;
+				printf("Current Scope: %s\n", currentScope->m_szName);
+				printf("\tClassDefinition: %s ( %i fields )\n", currentClassDef.m_pDeclaredClass->m_szName, numFields);
+				for (int fieldId = 0; fieldId < numFields; fieldId++) {
+					printf("Field: %s | Offset: 0x%x\n", classData->m_pFields[fieldId].m_szName, classData->m_pFields[fieldId].m_nOffset);
+				}
+			}
 		}
 	}
 	while (true) {
