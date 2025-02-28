6ff697af6364f28482dd4d6fe7a8c0e00bccb8e7
xsip <xsip@pm.me>
2025-02-28 17:00:36 +0100

    removes code duplication

--- CS2-SchemaSystem-Dumper/BaseLoader.hpp
+++ CS2-SchemaSystem-Dumper/BaseLoader.hpp
@@ -37,20 +37,7 @@ protected:
     }
 public:
     std::string *GetModuleNameFromPath() {
-        std::string* s = new std::string(mainDll);
-        std::string delimiter = "\\";
-        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-        std::string token;
-        std::vector<std::string> res;
-
-        while ((pos_end = s->find(delimiter, pos_start)) != std::string::npos) {
-            token = s->substr(pos_start, pos_end - pos_start);
-            pos_start = pos_end + delim_len;
-            res.push_back(token);
-        }
-
-        res.push_back(s->substr(pos_start));
-        return new std::string(res[res.size()-1]);
+        return BaseLoader::GetModuleNameFromPath(mainDll);
     }
     static std::string *GetModuleNameFromPath(const char* dllName) {
         std::string* s = new std::string(dllName);
